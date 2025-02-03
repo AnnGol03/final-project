0b08bd2058719c5414899fe106fe911879f42dba
AnnGol03 <annagolovina3111995@gmail.com>
2025-02-03 11:43:58 +0300

    sender_stand_requests.py

--- sender_stand_requests.py
+++ sender_stand_requests.py
@@ -1,8 +1,7 @@
-#Головина Анна,26 когорта,Финальный проекст- Инженер по тестированию плюс
+#Головина Анна,26 когорта,Финальный проекст- Инженер по тестированию плюс (автотест на этой вкладке)
 import requests
 import configuration
 import data
-import pytest
 
 def post_track_order(body):
      return requests.post(configuration.server_url + configuration.create_track,json=body,headers=data.headers)
@@ -12,6 +11,7 @@ def get_order(track):
    GET_ORDER_URL = f"{configuration.server_url}{configuration.get_order}"
    return requests.get(GET_ORDER_URL.format(track) ,headers=data.headers)
 
+#Автотест
 def test_create_and_get_order():
     response_create= post_track_order(data.track_body)
     assert response_create.status_code == 201 , "Ошибка при создании заказа"
